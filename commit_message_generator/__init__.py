2fc693714fc0d4c5d3b841cebd708e2bb3169740
nstraightbeam <157410854+nstraightbeam@users.noreply.github.com>
2025-03-13 19:54:22 -0400

    Delete commit_message_generator directory

--- commit_message_generator/__init__.py
+++
@@ -1,6 +0,0 @@
-# commit_message_generator/__init__.py
-from .emoji import emoji_commit
-from .style import style_commit
-from .haiku import haiku_commit
-
-__all__ = ['emoji_commit', 'style_commit', 'haiku_commit']
